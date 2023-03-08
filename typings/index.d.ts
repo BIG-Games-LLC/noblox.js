a0dc8ab4f8aee05fa7a7292f04077184f531eddc
Joe <joe.melsha@live.com>
2023-03-08 03:48:49 -0600

    fix incorrect result for getUniverseEngagementPayoutHistory

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -796,6 +796,10 @@ declare module "noblox.js" {
         eligibilityType: EligibilityType;
     }
 
+    type UniversePayoutHistoryResult = {
+        [date: string]: UniversePayoutHistoryData;
+    }
+
     /// Group
 
     type GroupIconSize = "150x150" | "420x420"
@@ -1586,7 +1590,7 @@ declare module "noblox.js" {
      * @param startDate - The first date in the range, specified as yyyy-MM-dd.
      * @param endDate - The last date in the range, specified as yyyy-MM-dd.
      */
-    function getUniverseEngagementPayoutHistory(universeId: number, startDate: string, endDate: string, jar?: CookieJar): Promise<UniversePayoutHistoryData[]>;
+    function getUniverseEngagementPayoutHistory(universeId: number, startDate: string, endDate: string, jar?: CookieJar): Promise<UniversePayoutHistoryResult>;
 
     // You can create a developer product, but the productId returned does not match the actual developer product id needed by the endpoints.
     // It's strange, but the edit link on the product page has the id that Roblox wants so you can edit dev products.
